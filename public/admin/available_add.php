93f9b04bda9226e0b5ad9c9bfdfd7e448abb4760
NadirZia <nadir.zia@pitb.gov.pk>
2025-05-23 21:35:43 +0500

    done

--- public/admin/available_add.php
+++ public/admin/available_add.php
@@ -21,19 +21,16 @@ ini_set('display_startup_errors', 1);
         // Create an array of not available dates
         $not_available_dates = explode(',', $not_available_dates_input);
         $not_available_dates = array_map('trim', $not_available_dates); // Trim whitespace
-        print_r($not_available_dates);
         // Skip if the current date is in the not available dates list
         foreach($not_available_dates as $item)
         {
-			echo "<br />";
-			 print_r($item);
+			
             $newUser = new Availability();
             $newUser->available_date = $item;  // Store full date
             $newUser->available_start_time = $available_start_time;
             $newUser->available_end_time = $available_end_time;
             $newUser->save();
         }
-		exit;
 
         
     
