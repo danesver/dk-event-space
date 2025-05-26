762d38291b4f5ce3336d07e9bfdd695557580a95
danesver <63498867+danesver@users.noreply.github.com>
2025-05-26 21:45:38 +0800

    Update 42 46 319 yt linkwelcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,11 +39,11 @@
                            <div class="rr-hero-banner-play d-flex align-items-center wow rrfadeUp"
                               data-wow-duration=".9s" data-wow-delay=".9s">
                               <div class="rr-hero-banner-play z-index-2">
-                                 <span><a class="popup-video" href="https://www.youtube.com/watch?v=BevD2sTa5Ds"><i
+                                 <span><a class="popup-video" href="https://www.youtube.com/embed/2IaEAzDgX3s?si=5d6Tv6sjdIxUEMbm"><i
                                           class="fa-sharp fa-regular fa-play"></i></a></span>
                               </div>
                               <div class="rr-hero-banner-text">
-                                 <b><a class="popup-video" href="https://www.youtube.com/watch?v=BevD2sTa5Ds">Watch
+                                 <b><a class="popup-video" href="https://www.youtube.com/embed/2IaEAzDgX3s?si=5d6Tv6sjdIxUEMbm">Watch
                                        Video</a></b>
                               </div>
                            </div>
@@ -316,7 +316,7 @@
                <div class="col-xl-12">
                   <div class="rr-video-wrap">
                      <div class="rr-video-play">
-                        <span><a class="popup-video" href="https://youtu.be/2IaEAzDgX3s?si=ea9Czq4QWVdqeX6T"><svg width="17"
+                        <span><a class="popup-video" href="https://www.youtube.com/embed/2IaEAzDgX3s?si=5d6Tv6sjdIxUEMbm"><svg width="17"
                                  height="22" viewBox="0 0 17 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                                  <path d="M1 1L16 11L1 21V11V1Z" stroke="#1B7262" stroke-width="2"
                                     stroke-linecap="round" stroke-linejoin="round" />
